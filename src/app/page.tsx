b54753848941d54c962921269bfa73fc969bd1bb
Raul Segui <rsegt@uoc.edu>
2024-11-05 19:42:55 +0100

    rename project and set auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import SignIn from "./components/sign-in";
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+        <SignIn/>
 
         <div className={styles.ctas}>
           <a
